1272a995e11bedbcbf2d96a2638c2bdc3e2897ea
Anton Peter <antonpeterp@email.com>
2026-06-16 16:08:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,13 @@ import postRouter from "./routes/post.routes.js";
 import notificationRouter from "./routes/notification.route.js";
 import passport from "passport";
 import { initPassport } from "./controller/auth.controller.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 initPassport();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let app = express();
 let httpServer = createServer(app);
@@ -56,14 +61,17 @@ app.use(
   })
 );
 app.use(passport.initialize());
-app.get("/", (req, res) => {
-  res.redirect("https://linkedin-clone-kmbx.onrender.com");
-});
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/notifications", notificationRouter);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
 
 connectDB().then(() => {
   httpServer.listen(process.env.PORT || 5000, () => {
